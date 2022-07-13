2775eb79841df76371b541a5c13a0d37d4d74a8a
chaitalinandkar <chaitalinandkar@gmail.com>
2022-07-13 14:14:48 -0700

    created react project and added all necessary changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Weather from "./Weather";
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <div className="weather-background">
+        <Weather />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,204 @@
+import React, { useState } from "react";
+import { BallTriangle } from 'react-loader-spinner';
+import axios from "axios";
+import "./TopSection.css";
+import "./MiddleSection.css";
+import "./BottomSection.css";
+import "./Footer.css";
+
+export default function TopSection() {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState("");
+  let [weather, setWeather] = useState("");
+
+  //find current date, year, day and time
+  let now = new Date();
+
+  //Current MONTH
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  let month = months[now.getMonth()];
+  
+
+  //Current DATE
+  let date = now.getDate();
+
+  //Current YEAR
+  let year = now.getFullYear();
+
+  //Display DATE format
+  let displayMonth = document.querySelector("#current-month");
+  displayMonth.innerHTML = `${month}`;
+  let displayDate = document.querySelector("#current-date");
+  displayDate.innerHTML = `${date},`;
+  let displayYear = document.querySelector("#current-year");
+  displayYear.innerHTML = `${year}`;
+
+  //Getting current DAY
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[now.getDay()];
+
+  //Getting current TIME
+  let hour = now.getHours();
+  let min = now.getMinutes();
+  let suffix;
+  if (hour >= 12 && min > 0) suffix = "PM";
+  else suffix = "AM";
+
+  if (hour > 12)
+    hour = hour - 12;
+  let minutes = (min <10?'0':'') + now.getMinutes();
+
+
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f7dffd4359849bb28c77fa4fe304c30f&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  let form = (<form>
+      <div className="heading">
+        <input
+          type="text"
+          placeholder="Enter a city..."
+          className="search-bar"
+          id="search-city"
+          onChange={updateCity}
+        />
+        <button className="search-button" id="search" onSubmit={handleSubmit}>
+          Search
+        </button>
+        <button className="current-location">
+          <i className="fa-solid fa-location-dot"></i>
+        </button>
+      </div>
+  </form>);
+  
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <div className="middle-section">
+        <div className="container" id="weather-container-left">
+        <div className="city" id="current-city">
+          {city}
+        </div>
+        <div className="date" id="display-date">
+          <strong>
+              <span id="current-month">{month}</span>
+          </strong>
+          <strong>
+            <span id="current-date">{date},</span>
+          </strong>
+          <span id="current-year">{year}</span>
+        </div>
+        <div className="day-time" id="day-time">
+          <strong>
+            <span className="displayed-day" id="current-day">
+               {day},
+            </span>
+          </strong>
+          <span className="time" id="current-time">
+             {hour}:{minutes}
+          </span>
+          <span className="am-pm" id="current-am-pm">
+            {suffix}
+          </span>
+        </div>
+      </div>
+
+      <div className="container" id="weather-container-middle">
+        <div className="weather-icon">
+          <img src={weather.icon} alt={weather.description} />
+        </div>
+        <div className="weather-description">{weather.description}</div>
+        <div className="temperature">
+          <div className="temp">
+            <div className="temp-number">
+              <strong>
+                <span className="current-temperature" id="display-temperature">
+                  25
+                </span>
+              </strong>
+            </div>
+          </div>
+          <div className="temp">
+            <div className="temp-unit">
+                {Math.round(weather.temperature)}&#xb0;
+              <a href="/" className="active" id="celsius-link">
+                C
+              </a>
+              |&#xb0;
+              <a href="/" id="fahrenheit-link">
+                F
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container" id="weather-container-right">
+        <div className="humidity">Humidity: {weather.humidity}%</div>
+        <div className="wind">Wind: {weather.wind} mph</div>
+      </div>
+      </div>
+      <div className="bottom-section" id="forecast"></div>
+      <div className="footer">
+        <p>
+          <a
+            href="https://github.com/chaitalinandkar"
+            className="open-source-code"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Chaitali Nandkar
+        </p>
+      </div>
+    </div>
+    );
+  } else {
+    return (
+    <BallTriangle
+      height="100"
+      width="100"
+      color='black'
+      ariaLabel='loading'
+    />
+    );
+  }
+  }
